790deaefbc22b2725c51a3bcb509db0ef324e8ed
ReaktorDTR <ReaktorDTR@gmail.com>
2014-09-29 12:57:17 +0300

    Completed

---
+++ src/com/javarush/test/level14/lesson08/home05/CompItem.java
@@ -0,0 +1,8 @@
+package com.javarush.test.level14.lesson08.home05;
+
+/**
+ * Created by Reaktor on 29.09.2014.
+ */
+public interface CompItem {
+    String getName();
+}
---
+++ src/com/javarush/test/level14/lesson08/home05/Mouse.java
@@ -0,0 +1,11 @@
+package com.javarush.test.level14.lesson08.home05;
+
+/**
+ * Created by Reaktor on 29.09.2014.
+ */
+public class Mouse implements CompItem {
+    @Override
+    public String getName() {
+        return "Mouse";
+    }
+}
