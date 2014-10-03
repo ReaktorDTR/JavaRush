945fab0f44fc27afa52899fb895803cb9c8914de
ReaktorDTR <ReaktorDTR@gmail.com>
2014-10-03 23:10:13 +0300

    Completed

---
+++ src/com/javarush/test/level16/lesson13/bonus01/ImageReaderFactory.java
@@ -0,0 +1,24 @@
+package com.javarush.test.level16.lesson13.bonus01;
+
+import com.javarush.test.level16.lesson13.bonus01.common.*;
+
+/**
+ * Created by Reaktor on 03.10.2014.
+ */
+public class ImageReaderFactory {
+    public static ImageReader getReader(ImageTypes type) {
+        ImageReader reader;
+
+        if (type == ImageTypes.JPG) {
+            reader = new JpgReader();
+        } else if (type == ImageTypes.BMP) {
+            reader = new BmpReader();
+        } else if (type == ImageTypes.PNG) {
+            reader = new PngReader();
+        } else {
+            throw new IllegalArgumentException("Неизвестный тип картинки");
+        }
+
+        return reader;
+    }
+}
---
+++ src/com/javarush/test/level16/lesson13/bonus01/common/JpgReader.java
@@ -0,0 +1,7 @@
+package com.javarush.test.level16.lesson13.bonus01.common;
+
+/**
+ * Created by Reaktor on 03.10.2014.
+ */
+public class JpgReader implements ImageReader {
+}
