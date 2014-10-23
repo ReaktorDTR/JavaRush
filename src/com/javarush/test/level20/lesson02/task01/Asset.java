e87aa970f6437536c3abac307fbaadbd99a41856
ReaktorDTR <ReaktorDTR@gmail.com>
2014-10-23 17:53:56 +0300

    Completed

--- src/com/javarush/test/level20/lesson02/task01/Asset.java
+++ src/com/javarush/test/level20/lesson02/task01/Asset.java
@@ -19,4 +19,27 @@ public class Asset {
     public void setPrice(double price) {
         this.price = price;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Asset)) return false;
+
+        Asset asset = (Asset) o;
+
+        if (Double.compare(asset.price, price) != 0) return false;
+        if (name != null ? !name.equals(asset.name) : asset.name != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        result = name != null ? name.hashCode() : 0;
+        temp = Double.doubleToLongBits(price);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
 }
--- src/com/javarush/test/level20/lesson02/task01/Solution.java
+++ src/com/javarush/test/level20/lesson02/task01/Solution.java
@@ -17,17 +17,20 @@ public class Solution {
         //вы можете найти your_file_name.tmp в папке TMP или исправьте outputStream/inputStream в соответствии с путем к вашему реальному файлу
         try {
 
-            File your_file_name = File.createTempFile("your_file_name", null);
-            OutputStream outputStream = new FileOutputStream(your_file_name);
-            InputStream inputStream = new FileInputStream(your_file_name);
+            File your_file_name = File.createTempFile("c:\\test.txt", null);
+            OutputStream outputStream = new FileOutputStream("c:\\test.txt");
+            InputStream inputStream = new FileInputStream("c:\\test.txt");
 
             Human ivanov = new Human("Ivanov", new Asset("home"), new Asset("car"));
             ivanov.save(outputStream);
             outputStream.flush();
+            outputStream.close();
 
             Human somePerson = new Human();
             somePerson.load(inputStream);
             //check here that ivanov equals to somePerson - проверьте тут, что ivanov и somePerson равны
+            if (ivanov.equals(somePerson)) System.out.println("true");
+            else System.out.println("false");
             inputStream.close();
 
         } catch (IOException e) {
@@ -55,11 +58,52 @@ public class Solution {
         }
 
         public void save(OutputStream outputStream) throws Exception {
-            //implement this method - реализуйте этот метод
+            PrintWriter writer = new PrintWriter(outputStream);
+            if (!this.name.isEmpty()) {
+                writer.println(this.name);
+                for (Asset element : assets) {
+                    writer.println(element.getName());
+                    writer.println(String.valueOf(element.getPrice()));
+                }
+            }
+            writer.println("EndOfHuman");
+            writer.flush();
+            writer.close();
         }
 
         public void load(InputStream inputStream) throws Exception {
-            //implement this method - реализуйте этот метод
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            if (reader.ready()) {
+                this.name = reader.readLine();
+                while (reader.ready()) {
+                    String str = reader.readLine();
+                    if (str.equals("EndOfHuman")) break;
+                    Asset asset = new Asset(str);
+                    asset.setPrice(Double.parseDouble(reader.readLine()));
+                    this.assets.add(asset);
+                }
+            }
+            reader.close();
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof Human)) return false;
+
+            Human human = (Human) o;
+
+            if (assets != null ? !assets.equals(human.assets) : human.assets != null) return false;
+            if (name != null ? !name.equals(human.name) : human.name != null) return false;
+
+            return true;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = name != null ? name.hashCode() : 0;
+            result = 31 * result + (assets != null ? assets.hashCode() : 0);
+            return result;
         }
     }
 }
