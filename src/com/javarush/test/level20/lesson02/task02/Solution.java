e1e58efdd0a93658d283f28ca8668fcec2db92f2
ReaktorDTR <ReaktorDTR@gmail.com>
2014-10-29 15:47:42 +0200

    Completed

--- src/com/javarush/test/level20/lesson02/task02/Solution.java
+++ src/com/javarush/test/level20/lesson02/task02/Solution.java
@@ -1,9 +1,10 @@
 package com.javarush.test.level20.lesson02.task02;
 
 import java.io.*;
-import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 /* Читаем и пишем в файл: JavaRush
 Реализуйте логику записи в файл и чтения из файла для класса JavaRush
@@ -16,8 +17,8 @@ public class Solution {
         //вы можете найти your_file_name.tmp в папке TMP или исправьте outputStream/inputStream в соответствии с путем к вашему реальному файлу
         try {
 //           File your_file_name = File.createTempFile("c:\\test.txt", null);
-            OutputStream outputStream = new FileOutputStream("D:\\test.txt");
-//            InputStream inputStream = new FileInputStream("c:\\test.txt");
+            OutputStream outputStream = new FileOutputStream("C:\\test.txt");
+            InputStream inputStream = new FileInputStream("c:\\test.txt");
 
             JavaRush javaRush = new JavaRush();
             SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
@@ -56,11 +57,15 @@ public class Solution {
             outputStream.flush();
 
             JavaRush loadedObject = new JavaRush();
-//            loadedObject.load(inputStream);
+            loadedObject.load(inputStream);
             //check here that javaRush object equals to loadedObject object - проверьте тут, что javaRush и loadedObject равны
 
             outputStream.close();
-//            inputStream.close();
+            inputStream.close();
+
+            if (javaRush.equals(loadedObject)) System.out.println("true");
+            else System.out.println("false");
+
 
         } catch (IOException e) {
             //e.printStackTrace();
@@ -76,22 +81,21 @@ public class Solution {
 
         public void save(OutputStream outputStream) throws Exception {
             PrintWriter writer = new PrintWriter(outputStream);
-            writer.println("StartBlockUsers");
-            for (User user : users)
-            {
+            for (User user : users) {
                 SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
-
+                StringBuilder sb = new StringBuilder();
                 System.out.println(user.getFirstName());
                 System.out.println(user.getLastName());
                 System.out.println(user.isMale());
                 System.out.println(dateFormat.format(user.getBirthDate()));
                 System.out.println(user.getCountry());
 
-                writer.println(user.getFirstName());
-                writer.println(user.getLastName());
-                writer.println(user.isMale());
-                writer.println(dateFormat.format(user.getBirthDate()));
-                writer.println(user.getCountry());
+                sb.append(user.getFirstName() + " ");
+                sb.append(user.getLastName() + " ");
+                sb.append(user.isMale() + " ");
+                sb.append(dateFormat.format(user.getBirthDate()) + " ");
+                sb.append(user.getCountry());
+                writer.println(sb.toString());
                 writer.flush();
             }
             writer.println("EndBlockUsers");
@@ -103,13 +107,49 @@ public class Solution {
             if (reader.ready()) {
                 while (reader.ready()) {
                     String str = reader.readLine();
-                    if (str.equals("EndOfHuman")) break;
-                    Asset asset = new Asset(str);
-                    asset.setPrice(Double.parseDouble(reader.readLine()));
-                    this.assets.add(asset);
+                    if (str.equals("EndBlockUsers")) break;
+                    String[] param = str.split(" ");
+                    User user = new User();
+                    user.setFirstName(param[0]);
+                    user.setLastName(param[1]);
+                    user.setMale(Boolean.parseBoolean(param[2]));
+                    SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+                    Date birthDate = dateFormat.parse(param[3]);
+                    user.setBirthDate(birthDate);
+                    switch (param[4]) {
+                        case "UKRAINE": {
+                            user.setCountry(User.Country.UKRAINE);
+                            break;
+                        }
+                        case "RUSSIA": {
+                            user.setCountry(User.Country.RUSSIA);
+                            break;
+                        }
+                        default: {
+                            user.setCountry(User.Country.OTHER);
+                        }
+                    }
+                    this.users.add(user);
                 }
             }
             reader.close();
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof JavaRush)) return false;
+
+            JavaRush javaRush = (JavaRush) o;
+
+            if (users != null ? !users.equals(javaRush.users) : javaRush.users != null) return false;
+
+            return true;
+        }
+
+        @Override
+        public int hashCode() {
+            return users != null ? users.hashCode() : 0;
         }
     }
 }
--- src/com/javarush/test/level20/lesson02/task02/User.java
+++ src/com/javarush/test/level20/lesson02/task02/User.java
@@ -65,4 +65,29 @@ public class User {
         }
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+
+        User user = (User) o;
+
+        if (isMale != user.isMale) return false;
+        if (birthDate != null ? !birthDate.equals(user.birthDate) : user.birthDate != null) return false;
+        if (country != user.country) return false;
+        if (firstName != null ? !firstName.equals(user.firstName) : user.firstName != null) return false;
+        if (lastName != null ? !lastName.equals(user.lastName) : user.lastName != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = firstName != null ? firstName.hashCode() : 0;
+        result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
+        result = 31 * result + (birthDate != null ? birthDate.hashCode() : 0);
+        result = 31 * result + (isMale ? 1 : 0);
+        result = 31 * result + (country != null ? country.hashCode() : 0);
+        return result;
+    }
 }
